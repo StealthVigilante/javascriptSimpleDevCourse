5273cf7c5c19c2f57f77c2f3b757310a5509ffd1
StealthVigilante <stephen.vish@gmail.com>
2026-04-15 04:31:32 +0530

    Day 1 learnt the basics of JavaScript Learnt basic Alert messages and what Syntax ,code and running the code means

---
+++ 1_JavaScript Basics/Notes 1.js
@@ -0,0 +1,18 @@
+// first go to inspect and console and type
+// 1. alert("Hello World");
+// 2. 2+2
+// > 4
+// 3. 10-3
+// >7
+// 4. document.body.innerHTML = 'hello';
+
+/* Basics of javascript
+-- giving instructions to a computer (code)
+-- the computer follows our instructions (running the code)
+
+Syntax
+--rules that we have  to follow when using a programming language
+-- similar to English (grammer)
+-- in programming, we have to follow the rules of syntax exactly
+
+*/
